a94ba21b0d3ef1363554a212ec09c22bfc4cd290
Eduardo Martini <emartini1984@gmail.com>
2024-10-16 12:13:11 +0200

    Adding manual, local, deploy

--- manualDeploy.sh
+++ manualDeploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 TARGET_BRANCH=gh-pages            # branch to commit to 
 
+cp -r _site _site_2deploy         # copy the _site folder to a temporary folder
 git fetch                         # fetch branches
 git checkout $TARGET_BRANCH       # checkout to your branch
-git rm -r *                        # remove all files from the branch
-git checkout master -- _site/*    # copy the files from the master branch
+rm -r _site                   # remove all files from the branch
+mv _site_2deploy/* .              # move the _site folder to the branch
+rm -r _site_2deploy           # remove all files from the branch
+git add -A
+git diff-index --quiet HEAD ||  git commit -am "deploy files"  # commit to the repository (ignore if no modification)
+git push origin $TARGET_BRANCH # push to remote branch
