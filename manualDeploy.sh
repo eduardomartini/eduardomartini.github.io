8ed0982c79103da3526b6363071fd9ae79684b58
Eduardo Martini <emartini1984@gmail.com>
2024-11-26 11:16:14 +0100

    deploy files

--- manualDeploy.sh
+++ manualDeploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 TARGET_BRANCH=gh-pages            # branch to commit to 
 
-cp -r _site _site_2deploy         # copy the _site folder to a temporary folder
 git fetch                         # fetch branches
 git checkout $TARGET_BRANCH       # checkout to your branch
-# rm -r _site                   # remove all files from the branch
-# mv _site_2deploy/* .              # move the _site folder to the branch
-# rm -r _site_2deploy           # remove all files from the branch
-find . -type d -d 1 -not -path "./_site*" -not -path "./.git*"  -exec rm -fr '{}' \; # remove all files from the branch
+find . -d 1 -not -path "./_site*" -not -path "./.git*"  -exec rm -fr '{}' \; # remove all files from the branch
 mv _site/* .                    # move the _site folder to the branch
 rm -r _site                     # remove _site folder
 git add -A
